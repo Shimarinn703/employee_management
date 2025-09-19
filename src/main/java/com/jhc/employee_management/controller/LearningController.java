7c3119fa6df5e282080fa86655ed448b2d0d9322
gitbluetrees <1793576488@qq.com>
2025-09-19 23:08:07 +0900

    王红梅：学习资源页面显示以及插入资源，前后端联动，后台数据查询更新没有问题，前端页面显示有问题

--- src/main/java/com/jhc/employee_management/controller/LearningController.java
+++ src/main/java/com/jhc/employee_management/controller/LearningController.java
@@ -1,16 +1,21 @@
 package com.jhc.employee_management.controller;
 
-import com.jhc.employee_management.common.ApiResponse;
-import com.jhc.employee_management.dto.LearningRequest;
-import com.jhc.employee_management.service.LearningResourcesService;
-import lombok.extern.slf4j.Slf4j;
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import com.jhc.employee_management.dto.PageResult;
+import com.jhc.employee_management.entity.LearningResources;
+import com.jhc.employee_management.service.LearningResourcesService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -19,14 +24,22 @@ public class LearningController {
 
     @Resource
     LearningResourcesService learningResourcesService;
-
-    @PostMapping("/creat")
-    public ResponseEntity<?> creat(@RequestBody LearningRequest LearningRequest) {
-        log.info("【creat】创建学习资料，用户名：{}", LearningRequest.getUsername());
-        learningResourcesService.creatLearning(LearningRequest);
-        log.info("【LOGIN】创建成功，用户名：{}", LearningRequest.getUsername());
-        return ResponseEntity.ok(ApiResponse.success("creat成功", null));
+    // GET: 分页获取学习资源 0918whm
+    @GetMapping("/show")
+    public ResponseEntity<PageResult<LearningResources>> getPagedResources(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+    	log.info("Received GET request for learning resources - page: {}, size: {}", page, size);
+        PageResult<LearningResources> result = learningResourcesService.getPagedResources(page, size);
+        log.debug("Returning {} resources", result.getItems().size());
+        return ResponseEntity.ok(result);
     }
-
-
+ // POST: 追加学习资源 0918whm
+    @PostMapping("/add")
+    public ResponseEntity<LearningResources> createResource(
+            @RequestBody LearningResources resource) {
+    	log.info("Received POST request to create resource: {}", resource.getTitle());
+        LearningResources createdResource = learningResourcesService.createResource(resource);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdResource);
+    }
 }
