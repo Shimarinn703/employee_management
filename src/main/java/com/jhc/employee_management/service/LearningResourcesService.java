1319d4c6bbcf73521b60dbf40ff8b3e020441a84
gitbluetrees <1793576488@qq.com>
2025-09-19 23:24:03 +0900

    王红梅：分页功能实现

--- src/main/java/com/jhc/employee_management/service/LearningResourcesService.java
+++ src/main/java/com/jhc/employee_management/service/LearningResourcesService.java
@@ -1,10 +1,11 @@
 package com.jhc.employee_management.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.jhc.employee_management.dto.LearningRequest;
-import com.jhc.employee_management.entity.Employee;
+import com.jhc.employee_management.dto.PageResult;
+import com.jhc.employee_management.entity.LearningResources;
 
-public interface LearningResourcesService extends IService<Employee> {
-
-    void creatLearning(LearningRequest learningRequest);
+public interface LearningResourcesService  {
+    //0918whm：分页
+	PageResult<LearningResources> getPagedResources(int page, int size);
+    LearningResources createResource(LearningResources resource);
+    int countAll();
 }
